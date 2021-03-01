796ad23398dcb10aef49db45f3183d2f14ef9629
Seth Hill <sethjoshuahill@gmail.com>
2021-03-01 13:57:55 -0700

    finished with the normal problems of classes

--- classes.js
+++ classes.js
@@ -30,6 +30,17 @@
 */
 
 //Code Here
+class Employee { 
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -48,6 +59,18 @@
 */
 
 //Code Here
+class Manager extends Employee{
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire(employee){
+    this.reports.push(employee)
+  }
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -72,6 +95,32 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  hire(employee){
+    super.hire(employee)
+    if(this.reports.length > 0 && this.reports.length < 4){
+      this.title = 'Barely Manager'
+    }else if(this.reports.length < 11){
+      this.title = 'Mostly Manager'
+    }else if(this.reports.length < 51){
+      this.title = 'Manager'
+    }else if(this.reports.length < 101){
+      this.title = 'Manager Plus'
+    }else if(this.reports.length >= 101){
+      this.title = 'Bestest Manager'
+    }
+
+  }
+  fire(index){
+    super.fire(index)
+    this.bonus += 100
+  }
+}
 
 
 
